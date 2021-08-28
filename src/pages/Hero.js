2759ba30316e9aac857b65e1c540a1272fa261bf
omdev420 <omdev420@gmail.com>
2021-08-28 21:49:23 +0530

    added loading and error state to the detail page

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
@@ -5,14 +6,42 @@ import { apiGet } from '../misc/config';
 const Hero = () => {
   const { id } = useParams();
   const [hero, setHero] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    apiGet(`${id}`).then(result => {
-      setHero(result);
-    });
+    let isMounted = true;
+
+    apiGet(`${id}`)
+      .then(result => {
+        setTimeout(() => {
+          if (isMounted) {
+            setHero(result);
+            setIsLoading(false);
+          }
+        }, 300);
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
   console.log(hero);
+
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
+
+  if (error) {
+    return <div>Error occurred: {error}</div>;
+  }
 
   return <div>Hero</div>;
 };
