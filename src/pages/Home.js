3cae494f9f9b1244955b2e3651239af0a00152aa
omdev420 <omdev420@gmail.com>
2021-08-28 16:01:44 +0530

    api fixed and on enter pressed added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,8 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
   const [input, setInput] = useState('');
-  let res;
-  let err = null;
 
   const onInputChange = event => {
     setInput(event.target.value);
@@ -14,19 +11,20 @@ const Home = () => {
   const onSearch = () => {
     // https://superheroapi.com/api/5995732007165992/search/name
     // ?maxResults=${maxResults}&startIndex=${startIndex}
-    axios
-      .get(
-        `https://superheroapi.com/api/5995732007165992/search/${input}`
-      )
-      .then(response => response.json())
+    fetch(
+      `https://superheroapi.com/api.php/5995732007165992/search/${input}`
+    )
+      .then(res => res.json())
       .then(
-        result => {
-          res = result;
-        },
-        error => {
-          err = error;
-        }
+        result => console.log(result),
+        error => console.log(error)
       );
+  };
+
+  const onKeyDown = event => {
+    if (event.key === 'Enter') {
+      onSearch();
+    }
   };
 
   return (
@@ -34,14 +32,12 @@ const Home = () => {
       <input
         type="text"
         onChange={onInputChange}
+        onKeyDown={onKeyDown}
         value={input}
       />
       <button type="button" onClick={onSearch}>
         Search
       </button>
-      <div>
-        Res: {res} Error: {err}
-      </div>
     </MainPageLayout>
   );
 };
