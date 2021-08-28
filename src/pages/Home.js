4fe91c7410f98d6e07b03371bc2d3703c869b129
omdev420 <omdev420@gmail.com>
2021-08-28 15:15:36 +0530

    dev deploy on vercel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  return <MainPageLayout>Home</MainPageLayout>;
+  const [input, setInput] = useState('');
+
+  const onInputChange = event => {
+    setInput(event.target.value);
+  };
+
+  const onSearch = () => {
+    // https://superheroapi.com/api/5995732007165992/search/name
+    // ?maxResults=${maxResults}&startIndex=${startIndex}
+    axios
+      .get(
+        `https://superheroapi.com/api/5995732007165992/search/${input}`
+        // {
+        //   headers: {
+        //     'access-control-allow-credentials': 'false',
+        //     'access-control-allow-headers':
+        //       'Origin, X-Requested-With, Content-Type, Accept',
+        //     'access-control-allow-methods': 'GET',
+        //     'access-control-allow-origin': '*',
+        //   },
+        // }
+      )
+      .then(response => response.json())
+      .then(
+        result => console.log(result),
+        error => console.log(error)
+      );
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
