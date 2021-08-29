ca2556cee73b259f65ff6d7c53497cb023451031
omdev420 <omdev420@gmail.com>
2021-08-29 17:05:27 +0530

    added custom hooks to add/remove id from the local storage of the browser

---
+++ src/misc/custom-hooks.js
@@ -0,0 +1,41 @@
+import { useReducer, useEffect } from 'react';
+
+function heroesReducer(prevState, action) {
+  switch (action.type) {
+    case 'ADD': {
+      return [...prevState, action.id];
+    }
+
+    case 'REMOVE': {
+      return prevState.filter(id => id !== action.id);
+    }
+
+    default:
+      return prevState;
+  }
+}
+
+export function usePresistedReducer(
+  reducer,
+  initialState,
+  key
+) {
+  const [state, dispatch] = useReducer(
+    reducer,
+    initialState,
+    inital => {
+      const persisted = localStorage.getItem(key);
+
+      return persisted ? JSON.parse(persisted) : inital;
+    }
+  );
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [state, key]);
+  return [state, dispatch];
+}
+
+export function useHeroes(key = 'heroes') {
+  return usePresistedReducer(heroesReducer, [], key);
+}
